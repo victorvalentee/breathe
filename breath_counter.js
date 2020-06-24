76540a6f563d0a1163ae1b3dccfe4a36f3691cfd
Victor Valente <victorvalentee@users.noreply.github.com>
2020-06-23 22:13:57 -0300

    Update breath_counter.js

--- breath_counter.js
+++ breath_counter.js
@@ -1,20 +1,22 @@
-var breathe = document.getElementById("breathe");
+window.onload = function () {
+  var breathe = document.getElementById("breathe");
 
-var breath_counter = 30;
+  var breath_counter = 30;
 
-breathe.addEventListener("animationStart", animationStart);
-breathe.addEventListener("animationIteration", animationRepeat);
-breathe.addEventListener("animationEnd", animationEnd);
+  breathe.addEventListener("animationStart", animationStart);
+  breathe.addEventListener("animationIteration", animationRepeat);
+  breathe.addEventListener("animationEnd", animationEnd);
 
-function animationStart() {
-  this.innerHTML = breath_counter;
-}
+  function animationStart() {
+    this.innerHTML = breath_counter;
+  }
 
-function animationRepeat() {
-  breath_counter--;
-  this.innerHTML = breath_counter;
-}
+  function animationRepeat() {
+    breath_counter--;
+    this.innerHTML = breath_counter;
+  }
 
-function animationEnd() {
-  this.innerHTML = "Have a great day!";
+  function animationEnd() {
+    this.innerHTML = "Have a great day!";
+  }
 }
