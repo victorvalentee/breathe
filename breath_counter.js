517b1edb5ab1baa5e11f31479fd91b15974e7b03
Victor Valente <victorvalentee@users.noreply.github.com>
2020-06-23 21:56:28 -0300

    Create breath_counter.js

---
+++ breath_counter.js
@@ -0,0 +1,20 @@
+var breathe = document.getElementById("breathe");
+
+var breath_counter = 30;
+
+breathe.addEventListener("webkitAnimationStart", animationStart);
+breathe.addEventListener("webkitAnimationIteration", animationRepeat);
+breathe.addEventListener("webkitAnimationEnd", animationEnd);
+
+function animationStart() {
+  this.innerHTML = breath_counter;
+}
+
+function animationRepeat() {
+  breath_counter--;
+  this.innerHTML = breath_counter;
+}
+
+function animationEnd() {
+  this.innerHTML = "Have a great day!";
+}
